a9a7a1b4777e99726225114a6ec0b767c725d859
Anton Lashchotka <a.lashchotka@knomary.com>
2025-06-18 03:14:51 +0300

    Change sample data

--- src/screens/Details/index.tsx
+++ src/screens/Details/index.tsx
@@ -16,31 +16,31 @@ import styles, { BUTTON_HEIGHT } from './styles';
 const ITEMS: IItem[] = [
   {
     id: 1,
-    title: 'Item',
-    description: 'Item description',
+    title: 'First item',
+    description: 'First item description',
     image:
       'https://images.unsplash.com/photo-1579353977828-2a4eab540b9a?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2FtcGxlfGVufDB8fDB8fHww',
   },
   {
     id: 2,
-    title: 'Item',
-    description: 'Item description',
+    title: 'Second item',
+    description: 'Second item description',
     image:
-      'https://images.unsplash.com/photo-1579353977828-2a4eab540b9a?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2FtcGxlfGVufDB8fDB8fHww',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjYSMQJToE4z85CJF5Zuey5tWS6FfapjEKMA&s',
   },
   {
     id: 3,
-    title: 'Item',
-    description: 'Item description',
+    title: 'Third item',
+    description: 'Third item description',
     image:
-      'https://images.unsplash.com/photo-1579353977828-2a4eab540b9a?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2FtcGxlfGVufDB8fDB8fHww',
+      'https://fujiframe.com/assets/images/_3000x2000_fit_center-center_85_none/10085/xhs2-fuji-70-300-Amazilia-Hummingbird.webp',
   },
   {
     id: 4,
-    title: 'Item',
-    description: 'Item description',
+    title: 'Forth item',
+    description: 'Forth item description',
     image:
-      'https://images.unsplash.com/photo-1579353977828-2a4eab540b9a?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2FtcGxlfGVufDB8fDB8fHww',
+      'https://photographylife.com/wp-content/uploads/2023/05/Nikon-Z8-Official-Samples-00002.jpg',
   },
 ];
 
